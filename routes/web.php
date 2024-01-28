b24b2d481c9f6202e2f4cd44974787abd6035d46
Yolan Mees <yolan.mees@icloud.com>
2024-01-28 03:05:33 +0000

    User Settings

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,10 @@ Route::get('/', function () {
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified' ])->group(function () {
 
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    });
+
     Route::get('/design', function () {
         return view('design');
     })->name('design');
