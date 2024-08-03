3c10a10fabce03501e6efdeb063e1f4686ea337a
Yolan Mees <yolan.mees@smart.be>
2024-08-04 00:21:50 +0200

    Wordpress installer dark mode

--- resources/views/site/wordpress/index.blade.php
+++ resources/views/site/wordpress/index.blade.php
@@ -13,79 +13,64 @@
             </div>
         @endif
         <div class="container-fluid">
-            <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
-                <div class="card mb-4 col-span-2 md:col-span-1">
-                    <div class="card-header">
-                        <i class="fab fa-wordpress fs-fw mr-1"></i>
-                        Create new Wordpress
-                    </div>
-                    <div class="card-body">
-                        <form class="flex flex-col" action="{{ route('site.wordpress.create', $site_id) }}" method="POST">
-                            @csrf
-                                <label class="my-2" for="path">Install path:</label>
-                                <input type="text" id="path" name="path" class="form-control" required>
-                                <label class="my-2" for="username">Wordpress Admin Username:</label>
-                                <input type="text" id="username" name="username" class="form-control" required>
-                                <label class="my-2" for="password">Wordpress Admin Password:</label>
-                                <input type="password" id="password" name="password" class="form-control" required>
-                            <button type="submit" class="btn btn-primary mt-2">Install Wordpress</button>
-                        </form>
+            <div class="grid grid-cols-1 gap-4 lg:grid-cols-2">
+                <x-card header="Create new Wordpress" size="md" dark="false">
+                       <form class="flex flex-col" action="{{ route('site.wordpress.create', $site_id) }}" method="POST">
+                        @csrf
+                        <label class="my-2" for="path">Install path:</label>
+                        <input type="text" id="path" name="path" class="form-control" required>
+                        <label class="my-2" for="username">Wordpress Admin Username:</label>
+                        <input type="text" id="username" name="username" class="form-control" required>
+                        <label class="my-2" for="password">Wordpress Admin Password:</label>
+                        <input type="password" id="password" name="password" class="form-control" required>
+                        <button type="submit" class="mt-2 btn btn-primary">Install Wordpress</button>
+                    </form>
 
-                        @if ($errors->any())
-                            <div class="alert alert-danger">
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
-
-                        @if (session('success'))
-                            <div class="alert alert-success">
-                                {{ session('success') }}
-                            </div>
-                        @endif
+                    @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
                     </div>
-                </div>
+                    @endif
 
-                <div class="card mb-4 col-span-2 md:col-span-1">
-                    <div class="card-header">
-                        <i class="fab fa-wordpress fs-fw mr-1"></i>
-                        List of Wordpress
+                    @if (session('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
                     </div>
-                    <div class="card-body">
-                        <table class="table table-bordered w-full">
-                            <thead>
-                                <tr>
-                                    <th>Path</th>
-                                    <th>Username</th>
-                                    <th>Actions</th>
+                    @endif
+                </x-card>
+                <x-card header="List of Wordpress" size="md" dark="false">
+                    <table class="table w-full table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Path</th>
+                                <th>Username</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($wordpresses as $wordpress)
+                                <tr class="text-center hover:bg-gray-100">
+                                    <td>{{ $wordpress->path }}</td>
+                                    <td>{{ $wordpress->username }}</td>
+                                    <td>
+                                        <form action="{{ route('site.wordpress.delete', $wordpress->id) }}"
+                                            method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger">Delete</button>
+                                        </form>
+                                    </td>
                                 </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($wordpresses as $wordpress)
-                                    <tr class="text-center hover:bg-gray-100">
-                                        <td>{{ $wordpress->path }}</td>
-                                        <td>{{ $wordpress->username }}</td>
-                                        <td>
-                                            <form action="{{ route('site.wordpress.delete', $wordpress->id) }}"
-                                                method="POST">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-danger">Delete</button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
+                            @endforeach
 
-                            </tbody>
-                        </table>
-                        {{ $wordpresses->links() }}
-                    </div>
-                      
-                </div>
-
+                        </tbody>
+                    </table>
+                    {{ $wordpresses->links() }}
+                </x-card>
 
             </div>
         </div>
