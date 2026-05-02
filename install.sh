d21df161b914472ec75ba664ceb7724866fd3432
Yolan Mees <yolan.mees@icloud.com>
2026-05-03 00:28:59 +0200

    fix: nginx try_files without / (403 on directory listing)

--- install.sh
+++ install.sh
@@ -113,7 +113,7 @@ server {
     server_name _;
     root /var/www/spikster/public;
     index index.php index.html;
-    location / { try_files $uri $uri/ /index.php?$query_string; }
+    location / { try_files $uri /index.php?$query_string; }
     location ~ \.php$ {
         fastcgi_pass unix:/var/run/php/php8.3-fpm.sock;
         fastcgi_param SCRIPT_FILENAME $realpath_root$fastcgi_script_name;
